6d6fbf8415eb8d9e8567db9ba68b8f5078b234b4
Yaroslav-Marinich <ysmarinich@gmail.com>
2023-07-17 13:50:47 +0300

    hw03-monodb

--- schemas/contactsSchema.js
+++ schemas/contactsSchema.js
@@ -6,17 +6,25 @@ const postSchema = Joi.object({
     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
     .required(),
   phone: Joi.number().required(),
+  favorite: Joi.boolean(),
 });
 
 const putSchema = Joi.object({
   name: Joi.string(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    ,
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
   phone: Joi.number(),
+  favorite: Joi.boolean(),
+});
+
+const patchSchema = Joi.object({
+  favorite: Joi.boolean().required(),
 });
 
 module.exports = {
   postSchema,
   putSchema,
+  patchSchema,
 };
