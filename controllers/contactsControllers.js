590757b32bed8efb351a2bb1bb6046b9d1d0071b
Yaroslav-Marinich <ysmarinich@gmail.com>
2023-07-18 10:19:42 +0300

    fix bug

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -42,8 +42,10 @@ const updateContact = async (req, res) => {
 };
 
 const updateFavorite = async (req, res) => {
-  const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
   if (!result) {
     throw new HttpError(404, "Not Found");
   }
